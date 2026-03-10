c8f0bda62cb32df6919424610605840b672b491f
Jeremy Huang <jeremyhuangofficial@gmail.com>
2026-03-10 12:42:19 -0700

    removed old commented code

--- app/components/TradingScreen.tsx
+++ app/components/TradingScreen.tsx
@@ -83,15 +83,6 @@ export default function TradingScreen() {
         {section === "trading" && <h2 className="text-2xl font-semibold leading-snug mb-6">Trading List</h2>}
         {section === "searching" && <h2 className="text-2xl font-semibold leading-snug mb-6">Searching List</h2>}
         {section === "selling" && <h2 className="text-2xl font-semibold leading-snug mb-6">Selling List</h2>}
-    
-        {/* probably should just remove this piece of text under ngl */}
-
-        {/* <div className="mb-6">
-          <h2 className="text-lg font-semibold">Cosmic Heroes :</h2>
-          <p className="text-gray-700">Saturday One Piece TCG Locals</p>
-        </div> */}
-
-
 
         <div className="grid grid-cols-3 mb-6">
           <button 
